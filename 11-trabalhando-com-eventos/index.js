7d7e3bdb72df01e3f350a7626741a5f27b583a9d
William Hespanha <willrhespanha2@gmail.com>
2024-09-04 09:19:48 -0300

    Utilizando o DOM no Java Script

---
+++ 11-trabalhando-com-eventos/index.js
@@ -0,0 +1,29 @@
+    function register (ev){
+        console.log(ev)
+        const sectionElement = ev.currentTarget.parentNode
+        const username = sectionElement.currentTarget.children.username.value
+        const password = sectionElement.currentTarget.children.password.value
+        const passwordConfirmation = sectionElement.children.passwordConfirmation.value
+
+        if(password === passwordConfirmation) {
+            alert('Usuário' + username + 'Registrado !')
+        }else{
+            alert('As senhas não conferem')
+        }
+    }
+    
+    const button = document.getElementById('register-button')
+
+    button.addEventListener('click',register )
+
+    function removeListener (){
+        button.removeEventListener('click', register)
+        alert('Event Removed')
+    }
+
+
+    button.addEventListener('mouseover', function(ev){
+        console.log(ev.currentTarget)
+    })
+
+    
